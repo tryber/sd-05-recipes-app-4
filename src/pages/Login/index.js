65ca433cbb90766f05ef8c57236c19fa6aa1e66c
Christian Duessel <duessel@gmail.com>
2020-09-08 20:21:22 -0300

    Login page with localStorage

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './index.css';
+import AppContext from '../../context/AppContext';
+import { Redirect } from 'react-router-dom';
 
-const Login = () => (
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [clickOk, setClickOk] = useState(false);
+
+  // const {setUser} = useContext(AppContext);
+
+  const emailregex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+
+  const clickSubmit = () => {
+    // setUser(email)
+    localStorage.setItem("user", {"email": email});
+    localStorage.setItem("mealsToken", 1);
+    localStorage.setItem("cocktailsToken", 1);
+    setClickOk(true);
+  }
+
+return(
+
   <div>
+    {clickOk && <Redirect to="/comidas" />}
     <h1 className="login">Login</h1>
     <form>
       <input
         data-testid="email-input"
         className="email"
-        type="type"
+        type="email"
         name="email"
         placeholder="Email"
+        onChange={ (e)=> setEmail(e.target.value) }
       />
       <input
         data-testid="password-input"
@@ -18,11 +40,14 @@ const Login = () => (
         type="password"
         name="password"
         placeholder="Senha"
+        onChange={ (e)=> setSenha(e.target.value) }
       />
       <button
+        type="button"
         data-testid="login-submit-btn"
         className="login-btn"
-        onClick={() => console.log('Supimpa baby')}
+        disabled={!(email.match(emailregex) && senha.length >= 6)}
+        onClick={() => clickSubmit()}
         value="Entrar"
       >
         Entrar
@@ -30,5 +55,6 @@ const Login = () => (
     </form>
   </div>
 );
+}
 
 export default Login;
