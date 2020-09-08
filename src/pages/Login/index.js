8c6118c1e86767f5cf97645c0cacd66754af1072
ederusena <eder.sena@live.com>
2020-09-08 18:09:20 -0300

    Acertos CC

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,12 +3,25 @@ import './index.css';
 
 const Login = () => (
   <div>
-    <h1>Login</h1>
+    <h1 className="login">Login</h1>
     <form>
-      <input className="email" type="type" name="email" placeholder="Email" />
-      <input className="password" type="password" name="password" placeholder="Senha" />
+      <input
+        data-testid="email-input"
+        className="email"
+        type="type"
+        name="email"
+        placeholder="Email"
+      />
+      <input
+        data-testid="password-input"
+        className="password"
+        type="password"
+        name="password"
+        placeholder="Senha"
+      />
       <button
-        className='login-btn'
+        data-testid="login-submit-btn"
+        className="login-btn"
         onClick={() => console.log('Supimpa baby')}
         value="Entrar"
       >
