43019c748c48e80dd3e96e83aebf696d02243e5b
ederusena <eder.sena@live.com>
2020-09-08 17:45:36 -0300

    Tela-login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import './index.css';
 
 const Login = () => (
   <div>
     <h1>Login</h1>
+    <form>
+    <input
+    id='email'
+      type="text"
+      name="email"
+      placeholder="Email"
+    />
+    <input
+      id='password'
+      type="text"
+      name="password"
+      placeholder="Senha"
+    />
+    <button
+      classname="login-btn"
+      onClick={() => console.log('Supimpa baby')}
+      value="Entrar"
+    >
+        Entrar
+    </button>
+    </form>
   </div>
 );
 
