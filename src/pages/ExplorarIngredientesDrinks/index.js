7023713663f07a0e9a82eac0ec0a2f6b3d4a0817
Christian Duessel <duessel@gmail.com>
2020-09-11 21:17:00 -0300

    CC

--- src/pages/ExplorarIngredientesDrinks/index.js
+++ src/pages/ExplorarIngredientesDrinks/index.js
@@ -7,30 +7,35 @@ import { getDrinksIngredients, getDrinksByIngredients } from '../../services/Dri
 
 const ExplorarIngredientesDrinks = () => {
   const [ingArray, setIngArray] = useState([]);
-  const [redirect, setRedirect] = useState(false);
-  useEffect(() => {getDrinksIngredients().then((data) => setIngArray(data.drinks))}, []);
+  const [redirect, setRed] = useState(false);
+  useEffect(() => { getDrinksIngredients().then((data) => setIngArray(data.drinks)) }, []);
 
   const { setDataDrink } = useContext(AppContext);
 
   const clickOn = (ingrediente) => {
-    getDrinksByIngredients(ingrediente).then((data) => {setDataDrink(data.drinks); setRedirect(true)})
+    getDrinksByIngredients(ingrediente).then((data) => { setDataDrink(data.drinks); setRed(true) });
   };
 
-  const listIngredients = (ingArray) =>
+  const listIngredients = () =>
   ingArray.map((ingrediente, index) => (
     <div
       className="recipe-card"
       key={ingrediente.strDrink}
       data-testid={`${index}-ingredient-card`}
-      onClick={()=> clickOn(ingrediente.strIngredient1)}
     >
       <img
         className="thumbnail"
         data-testid={`${index}-card-img`}
         src={`https://www.thecocktaildb.com/images/ingredients/${ingrediente.strIngredient1}-Small.png`}
         alt="thumbnail do ingrediente"
+        onClick={()=> clickOn(ingrediente.strIngredient1)}
       />
-      <p data-testid={`${index}-card-name`}>{ingrediente.strIngredient1}</p>
+      <p 
+        data-testid={`${index}-card-name`}
+        onClick={() => clickOn(ingrediente.strIngredient1)}
+      >
+        {ingrediente.strIngredient1}
+      </p>
     </div>
   ));
 
@@ -38,11 +43,11 @@ const ExplorarIngredientesDrinks = () => {
   if (redirect) return <Redirect to="/bebidas" />;
 
   return (
-  <div className="recipes-container">
-    <Header hideSearch>Explorar Ingredientes</Header>
-    {listIngredients(ingArray)}
-    <Footer />
-  </div>
+    <div className="recipes-container">
+      <Header hideSearch>Explorar Ingredientes</Header>
+      {listIngredients(ingArray)}
+      <Footer />
+    </div>
   );
 };
 
