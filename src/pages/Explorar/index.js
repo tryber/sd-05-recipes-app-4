25db860153d2a5ce2053bb41265a2f4b79e0a43c
Christian Duessel <duessel@gmail.com>
2020-09-16 16:35:31 -0300

    CC

--- src/pages/Explorar/index.js
+++ src/pages/Explorar/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Spoon from '../../images/spoon.png';
-import Drink from '../../images/martini.png'
+import Drink from '../../images/martini.png';
 
 const Explorar = () => (
   <div className="main">
     <Header hideSearch>Explorar</Header>
     <div className="middle">
       <Link to="/explorar/comidas" data-testid="explore-food" className="btn">
-        <img src={Spoon} alt="spoon pic" className="btnPic"/>
+        <img src={Spoon} alt="spoon pic" className="btnPic" />
         Explorar Comidas
       </Link>
       <Link to="/explorar/bebidas"data-testid="explore-drinks" className="btn">
-        <img src={Drink} alt="spoon pic" className="btnPic2"/>
+        <img src={Drink} alt="spoon pic" className="btnPic2" />
         Explorar Bebidas
       </Link>
     </div>
