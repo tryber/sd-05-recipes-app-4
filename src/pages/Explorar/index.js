297a6c05e22821abce94398e6d01b6cbb6463020
Christian Duessel <duessel@gmail.com>
2020-09-16 16:17:59 -0300

    layout tela explorar

--- src/pages/Explorar/index.js
+++ src/pages/Explorar/index.js
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 import './index.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Spoon from '../../images/spoon.png';
+import Drink from '../../images/martini.png'
 
 const Explorar = () => (
-  <div>
+  <div className="main">
     <Header hideSearch>Explorar</Header>
-    <Link to="/explorar/comidas" data-testid="explore-food" className="btn">Explorar Comidas</Link>
-    <Link to="/explorar/bebidas"data-testid="explore-drinks" className="btn">Explorar Bebidas</Link>
+    <div className="middle">
+      <Link to="/explorar/comidas" data-testid="explore-food" className="btn">
+        <img src={Spoon} alt="spoon pic" className="btnPic"/>
+        Explorar Comidas
+      </Link>
+      <Link to="/explorar/bebidas"data-testid="explore-drinks" className="btn">
+        <img src={Drink} alt="spoon pic" className="btnPic2"/>
+        Explorar Bebidas
+      </Link>
+    </div>
     <Footer />
   </div>
 );
