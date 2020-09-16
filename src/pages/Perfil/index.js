fcbaebe665d2d8b04770d859a2dccc8f80a9a2a4
Christian Duessel <duessel@gmail.com>
2020-09-16 17:58:25 -0300

    profile Layout

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -3,6 +3,9 @@ import { Link, Redirect } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import './index.css';
+import Tick from '../../images/orngeTick.png';
+import Heart from '../../images/newOrangeHeart.png';
+import Exit from '../../images/exit.png';
 
 
 const Perfil = () => {
@@ -17,7 +20,7 @@ const Perfil = () => {
   };
 
   return (
-    <div>
+    <div className="mainProfile">
       <Header hideSearch>Perfil</Header>
       {clickOk && <Redirect to="/" />}
       {userEmail &&
@@ -25,12 +28,15 @@ const Perfil = () => {
         <p data-testid="profile-email" className="txt">{userEmail}</p>
         <div className="btns">
           <Link className="botao" to="/receitas-feitas" data-testid="profile-done-btn" >
+            <img src={Tick} alt="tick pic" className="btnPic" />
             Receitas Feitas
           </Link>
           <Link className="botao" to="/receitas-favoritas" data-testid="profile-favorite-btn" >
+            <img src={Heart} alt="heart pic" className="btnPic" />
             Receitas Favoritas
           </Link>
           <Link className="botao" to="/" data-testid="profile-logout-btn" onClick={() => clickF()}>
+            <img src={Exit} alt="exit pic" className="btnPic" />
             Sair
           </Link>
         </div>
