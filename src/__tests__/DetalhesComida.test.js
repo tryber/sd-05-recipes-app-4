91e7680732f6cd564fa80af3397b95920fd701a3
Felipe Vieira <flpvieira86@gmail.com>
2020-09-17 22:37:42 -0300

    testes iniciais DetalhesComida

--- src/__tests__/DetalhesComida.test.js
+++ src/__tests__/DetalhesComida.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, waitFor, getByTestId } from '@testing-library/react';
+import { fireEvent, waitFor } from '@testing-library/react';
 import renderWithRouter from '../__render__/renderWithRouter';
 import DetalheComida from '../pages/DetalhesComida';
 
@@ -40,5 +40,28 @@ describe('Testes na tela de detalhes da comida', () => {
     await waitFor(() => expect(mealApi.getMealById).toHaveBeenCalled());
     const category = getByTestId('recipe-category');
     expect(category).toHaveTextContent('Vegetarian');
-  })
+  });
+
+  it('As intruções aparecem corretamente', async () => {
+    const { getByTestId } = renderWithRouter(
+      <DetalheComida match={{ params: { id: '52771' } }} />,
+      ['/comidas/52771'],
+    );
+    await waitFor(() => expect(mealApi.getMealById).toHaveBeenCalled());
+    const instructions = getByTestId('instructions');
+    expect(instructions).toHaveTextContent(
+      'Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes. In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil. Drain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.',
+    );
+  });
+
+  it('Ao pressionar o botão de iniciar receita, a página é redirecionada para a página correta', async () => {
+    const { history, getByTestId } = renderWithRouter(
+      <DetalheComida match={{ params: { id: '52771' } }} />,
+      ['/comidas/52771'],
+    );
+    await waitFor(() => expect(mealApi.getMealById).toHaveBeenCalled());
+    const btn = getByTestId('start-recipe-btn');
+    fireEvent.click(btn);
+    expect(history.location.pathname).toBe('/comidas/52771/in-progress');
+  });
 });
